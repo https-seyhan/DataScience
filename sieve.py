07dff127400b46aab22382262ff5273dd71da670
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-16 10:40:24 +1000

    Add files via upload

---
+++ sieve.py
@@ -0,0 +1,35 @@
+from math import sqrt
+import csv
+import os
+
+def sieve(N):
+	# Check current working directory.
+	#retval = os.getcwd()
+	#print ("Current working directory %s" % retval)
+	os.chdir("/home/saul/")
+	#print(N)
+	#N = 100
+	is_prime = (N + 1) * [True]
+	#print('Is Prime   :', is_prime)
+	#print(N, " ",int(sqrt(N)))
+	for candidate in range(2, int(sqrt(N)) + 1):
+		#print(N, " ",int(sqrt(N)))
+		if is_prime[candidate]:
+            
+            #print(candidate)
+		   for witness in range(candidate * candidate, N + 1, candidate):
+               is_prime[witness] = False
+	return is_prime[N]
+
+
+if __name__ == '__main__':
+    primeNumbers = []
+	#main()
+	#print('******')w
+	#writer = csv.writer(open("sieve.csv", 'w'))
+	for candidate in range(10, 100):
+		if sieve(candidate):
+			#print('******')
+			print(candidate)
+			#writer.writerow([candidate])
+            
