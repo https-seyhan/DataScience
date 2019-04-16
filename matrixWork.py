9f1940ab692ec5bbb75311908fa99cbdcd998157
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-16 10:43:57 +1000

    Add files via upload

---
+++ matrixWork.py
@@ -0,0 +1,26 @@
+from numpy import matrix
+from numpy import linalg
+
+def createMatrix():
+	A = matrix([[1,2,3], [11,12,13], [21, 22,23]])
+	x = matrix( [[1],[2],[3]] )
+	y = matrix( [[1,2,3]] ) 
+	print('Matrix of A :',A)
+	#print(A*x)
+	calculateMatrix(A, x, y)
+
+def calculateMatrix(A,x,y):
+	multip = A*x
+	print(A + y)
+	print(A*x)
+	print('Multiplication :', multip)
+	print('Transpose of A : ', A.T)
+	print('Inverse of A :', A.I)
+	print ('Solve Linear Equation of A an X :', linalg.solve(A, x))
+	print(A[2,1])
+
+
+
+if __name__ == '__main__':
+	createMatrix()
+	#calculateMatrix()
