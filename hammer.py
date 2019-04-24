35356d43c8cc86d21a90a6891292527a925311b8
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-04-24 16:57:14 +1000

    Add files via upload

---
+++ hammer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 23:29:45 2019
+
+@author: saul
+"""
+
+import os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import pymc3 as pm
+
+
+os.chdir("/home/saul/pythonWork")
+
+#textfile = pd.read_csv("txtdata.csv", sep=",", header=None)
+textfile = np.loadtxt("txtdata.csv")
+
+#print(textfile.describe())
+
+#@pm.deterministic
+
+size = len(textfile)
+print(size)
+alpha = 1 / size
+alpha = 1.0 / textfile.mean()
+
+
+
+with pm.Model() as model:
+    lambda_1 = pm.Exponential('lambda_1', alpha)
+    lambda_2 = pm.Exponential('lambda_2', alpha)
+    tau = pm.DiscreteUniform("tau", lower=0, upper=size)
+
+
+
+with pm.Model():
+    x = pm.Normal('x', mu=0, sd=1)
+    plus_2 = pm.Deterministic('x plus 2', x + 2)
+    print("X: ", plus_2)
+
+
+def lambda_ (tau=tau, lambda_1 = lambda_1, lambda_2 = lambda_2):
+    out = np.zeros(size)
+    out[:tau] = lambda_1
+    out[tau:] = lambda_2
+    
+print(lambda_(tau.random(), lambda_1.random(), lambda_2.random()))
+
+
+    
+
