20cab1a6a818a831a6bab3b376bf4f63556aa7d9
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-05-10 00:44:50 +1000

    Add files via upload

---
+++ montecarlo.py
@@ -0,0 +1,58 @@
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+
+def rollDice():
+	roll = random.randint(1,100)
+	if roll == 100:
+		#print roll, 'roll was 100'
+		return False
+	elif roll <= 50:
+		#print roll, 'roll was 1-50'
+		return False
+
+	elif 100 > roll > 50:
+		#print roll, 'roll was 51-99. You win'
+		return True
+
+def simple_bettor(funds, initial_wager, wager_count):
+	value = funds
+	wager = initial_wager
+
+	wX = []
+	vY = []
+
+	
+	current_Wager = 1
+
+	while current_Wager < wager_count:
+		if rollDice():
+			value += wager
+			wX.append(current_Wager)
+			vY.append(value)
+		else:
+			value -= wager
+			wX.append(current_Wager)
+			vY.append(value)
+
+		
+		current_Wager +=1
+
+	if value < 0:
+		value = 'Broke'
+	elif value > 0:
+		value = 'Win'
+	elif value == 0:
+		value = 'Equal'
+	#print 'Funds:', value
+
+	plt.plot(wX, vY) #Plot data
+
+x = 0
+while  x < 100:
+	simple_bettor(10000,100,100000)
+	x += 1
+
+plt.ylabel('Account Value')
+plt.xlabel('Wager Count')
+plt.show()
