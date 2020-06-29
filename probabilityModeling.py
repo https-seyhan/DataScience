0ab918c46acd894abeb18ad95d4152f4d670943c
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Python | SAS | R <sauldata@tutanota.com>
2020-06-29 22:42:22 +1000

    Add files via upload

---
+++ probabilityModeling.py
@@ -0,0 +1,37 @@
+from random import *
+import numpy as np
+from matplotlib import  pyplot as plt
+
+print("Start")
+
+class probabilityModel:
+    randomVariable = []
+    def __init__(self):
+        print("Constructor Called!!")
+
+        randomVariable = np.random.rand(100)
+        #randomVariable = random()
+        self.__plotProbs(randomVariable)
+        #print("random Variable", randomVariable)
+
+    def generateModel(self):
+        print("Generate Model")
+
+    def __plotProbs(self, randomVal):
+        print(randomVal)
+
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        print("Max value ", max(randomVal))
+        print("Mean value ", np.mean((randomVal)))
+        ax1.set_xlabel('Numbers')
+        ax1.set_ylabel('Log value of random number [0,1]')
+        ax2.set_xlabel('Numbers')
+        ax2.set_ylabel('Value of random number [0,1]')
+        ax1.plot(np.log(randomVal))
+        ax2.plot(randomVal)
+        plt.show()
+
+
+if __name__ == '__main__':
+    probm = probabilityModel()
+    #probm.generateModel()
