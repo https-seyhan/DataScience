99fd5f996bb3e91a278436241937043ac092ddda
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2020-12-18 09:51:19 +1100

    Update sieve.py

--- sieve.py
+++ sieve.py
@@ -24,7 +24,6 @@ def sieve(N):
 if __name__ == '__main__':
     primeNumbers = []
 	#main()
-	#print('******')w
 	#writer = csv.writer(open("sieve.csv", 'w'))
 	for candidate in range(10, 100):
 		if sieve(candidate):
