560efbcba2f13ba531268424952f77b198944295
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-08 09:49:39 +1100

    Update poissondist_visualization.py

--- poissondist_visualization.py
+++ poissondist_visualization.py
@@ -17,7 +17,6 @@ print(colors)
 plt.bar(a, poi.pmf(a, lambda_[0]), color=colors[0], label="$\lambda_ = %.1f$" % lambda_[0], alpha = 0.60, 
         edgecolor= colors[0], lw="3")
 
-
 plt.bar(a, poi.pmf(a, lambda_[1]), color=colors[1], label="$\lambda_ = %.1f$" % lambda_[1], alpha = 0.60, 
         edgecolor= colors[0], lw="3")
 
