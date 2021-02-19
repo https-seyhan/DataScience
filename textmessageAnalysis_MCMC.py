30283f498b55f32f9aeb819f8f8440070078ea49
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-02-19 17:02:23 +1100

    Update textmessageAnalysis_MCMC.py

--- textmessageAnalysis_MCMC.py
+++ textmessageAnalysis_MCMC.py
@@ -3,6 +3,8 @@
 """
 @author: saul
 """
+# PyMC3 is a Python package for Bayesian statistical modeling and probabilistic machine learning 
+# which focuses on advanced Markov chain Monte Carlo and variational fitting algorithms
 import os
 import pandas as pd
 import numpy as np
