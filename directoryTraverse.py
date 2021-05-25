c9e75f971f7cf5f1567b58f9988af849ca8024e1
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-05-25 19:08:41 +1000

    Update directoryTraverse.py

--- directoryTraverse.py
+++ directoryTraverse.py
@@ -8,7 +8,7 @@ import os
 def list_files(dir):                                                                                                
     r = []                                                                                                          
     subdirs = [x[0] for x in os.walk(dir)]                                                                            
-    for subdir in subdirs:                                                                                         
+    for subdir in subdirs:                                                                                        
         files = os.walk(subdir).next()[2]                                                                             
         if (len(files) > 0):                                                                                   
             for file in files:                                                                                        
