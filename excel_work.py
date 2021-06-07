5b8364169d0a94c9f4e385fda18fb090ae8b8d40
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-07 20:05:53 +1000

    Update excel_work.py

--- excel_work.py
+++ excel_work.py
@@ -24,7 +24,7 @@ def excel_work():
      for row in worksheet.iter_rows(values_only=True):
          print(row)
      #for row in worksheet.rows:
-    
+   
 
 if __name__ == '__main__':
     #test()
