9461ae3fe381713e39ad712de263e417f3edd42c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-09 21:59:23 +1000

    Update excel_work.py

--- excel_work.py
+++ excel_work.py
@@ -22,7 +22,7 @@ def excel_work():
      column_count = worksheet.max_column
      print("Row count ", row_count)
      print("Column ", column_count)
-     #for row in worksheet.iter_rows(values_o
+     #for row in worksheet.iter_rows(values_
      worksheet.cell(row=33, column=2).value = 'https://www.softwaretestinghelp.com/python-openpyxl-tutorial/'
      worksheet.cell(row=34, column=2).value = 'https://openpyxl.readthedocs.io/en/latest/pandas.html'
      worksheet.insert_rows(13)
