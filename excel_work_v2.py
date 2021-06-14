9a88b4547a194995770c83845d979f3c8dc36df0
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-14 21:39:44 +1000

    Update excel_work_v2.py

--- excel_work_v2.py
+++ excel_work_v2.py
@@ -65,7 +65,7 @@ def update_formulas(workbook, worksheet, column_label, column, funct, row_shift,
     print("Column ", column)
 
     print("Word ", word, '\n')
-    
+   
     #worksheet[column_coordinate]= '=' + funct + '(' + column + str(new_start_row) + ':' + column + str(new_end_row) + ')'
     
     worksheet.cell(row= row_location, column=column).value = word
