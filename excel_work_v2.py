866bc30d85899785558161d691730e47d1e4519a
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-16 21:01:14 +1000

    Update excel_work_v2.py

--- excel_work_v2.py
+++ excel_work_v2.py
@@ -35,7 +35,7 @@ def excel_work():
      worksheet.insert_rows(13)
      workbook.save("sample.xlsx")
      print("Saved")
-     #for row in worksheet.rwsprint (row)
+     #for row in worksheet.rwsprint (row
 
 def get_function_ranges(cellFormula):
     print("Cell formula ", cellFormula)
