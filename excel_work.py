b86ccf2b0fb5c6f1e7e8822f64d57f948f9a7739
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-19 10:34:34 +1000

    Update excel_work.py

--- excel_work.py
+++ excel_work.py
@@ -100,7 +100,7 @@ def copy_formula():
     #print(value)
     row_count = worksheet.max_row
     column_count = worksheet.max_column
-  
+ 
     # insert insert three roe at row three
     row_shift_pos = 5
     row_shift = 3
