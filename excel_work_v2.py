15c16e1a28592c85556711371d81a5ef99efbcfd
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-20 10:59:24 +1000

    Update excel_work_v2.py

--- excel_work_v2.py
+++ excel_work_v2.py
@@ -82,7 +82,7 @@ def count_dates(workbook, worksheet):
     for traverse_row in range(row_location, row_location + 1): 
         #for column in "CDEFGHJI":  
         while column <= worksheet.max_column:
-     
+    
             cell_name = "{}{}".format(column, traverse_row)
             #print("Cell Name ", cell_name)
             #print("Col values ", worksheet.cell(row=traverse_row, column=column).value) # the value of the specific cell
