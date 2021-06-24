16e6c7441462f425391136489fd50b43878354ff
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-24 21:49:48 +1000

    Update excel_work_v2.py

--- excel_work_v2.py
+++ excel_work_v2.py
@@ -15,7 +15,7 @@ def test():
     sheet["A1"] = "hello"
     sheet["B1"] = "world!"
     workbook.save(filename="hello_world.xlsx")
-    
+   
 def excel_work():
      workbook = load_workbook(filename="sample.xlsx")
      worksheet = workbook['Sheet2']
