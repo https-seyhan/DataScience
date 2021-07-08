01ab36849459809b7904f429ee2f6e9b9157eebb
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-07-08 20:19:49 +1000

    Update textmessageAnalysis_MCMC.py

--- textmessageAnalysis_MCMC.py
+++ textmessageAnalysis_MCMC.py
@@ -34,7 +34,7 @@ with pm.Model() as model:
 n_data_points = size
 idx = np.arange(n_data_points)
 with model:
-    lambda_ = pm.math.switch(tau >= idx, lambda_1, lambda_2)    
+    lambda_ = pm.math.switch(tau >= idx, lambda_1, lambda_2)   
 
 #pm.Poisson("obs", lambda_, value = textfile, observed=True)
 with model:
