2af64f48ac7908fb7ce339c4677de48b945b8127
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-09 09:52:30 +1000

    Update Automated_Excel_Update.py

--- Automated_Excel_Update.py
+++ Automated_Excel_Update.py
@@ -84,7 +84,7 @@ def count_dates(workbook, worksheet):
             #print("Cell Name ", cell_name)
             #print("Col values ", worksheet.cell(row=traverse_row, column=column).value) # the value of the specific cell
             # if there is new rows inserted
-    
+   
             if row_shifts > 0:
                 funct, ranges, start, end = get_function_ranges(worksheet.cell(row=traverse_row, column=column).value )
                 #get integer part
