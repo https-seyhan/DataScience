755c4c14511791ad978ce8f8aa9f87cd2266f1d3
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-09 11:17:16 +1000

    Update textmessageAnalysis_MCMC.py

--- textmessageAnalysis_MCMC.py
+++ textmessageAnalysis_MCMC.py
@@ -26,6 +26,7 @@ plt.title("Did the user's texting habit change over time?")
 plt.xlim(0, size)
 
 # Create the MCMC model with its parameters (lambdas, tau)
+
 with pm.Model() as model:
     lambda_1 = pm.Exponential('lambda_1', alpha) # create stochastic variable
     lambda_2 = pm.Exponential('lambda_2', alpha) #create stochastic variable
