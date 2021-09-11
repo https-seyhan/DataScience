78669789f57bc3baddff3616dda9a261aabb5793
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-11 14:08:27 +1000

    Update Automated_Excel_Update.py

--- Automated_Excel_Update.py
+++ Automated_Excel_Update.py
@@ -80,7 +80,6 @@ def count_dates(workbook, worksheet):
     for traverse_row in range(row_location, row_location + 1): 
         while column <= worksheet.max_column:
             cell_name = "{}{}".format(column, traverse_row)
-
             #print("Cell Name ", cell_name)
             #print("Col values ", worksheet.cell(row=traverse_row, column=column).value) # the value of the specific cell
             # if there is new rows inserted
