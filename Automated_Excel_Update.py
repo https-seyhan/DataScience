885d160730465f4d3d03174ca9d93b0dae231cd9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-09-23 15:44:20 +1000

    Update Automated_Excel_Update.py

--- Automated_Excel_Update.py
+++ Automated_Excel_Update.py
@@ -110,7 +110,7 @@ def copy_formula():
     column_count = worksheet.max_column
     #print("Row count ", row_count)
     #print("Column ", column_count)
-     #Add formula 
+    #Add formula 
     worksheet["D7"] = "=AVERAGE(D9:D26)"
     workbook.save("sample_v3.xlsx")
 
