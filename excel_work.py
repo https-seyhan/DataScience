4404df12846710a8afe923638de93465047b38ea
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-01-06 12:23:14 +1100

    Update excel_work.py

--- excel_work.py
+++ excel_work.py
@@ -10,7 +10,7 @@ from openpyxl.utils import get_column_letter
 os.chdir('/home/saul/Business')
 
 def test():
-    print("Test")
+    
     workbook = Workbook()
     sheet = workbook.active
     sheet["A1"] = "hello"
