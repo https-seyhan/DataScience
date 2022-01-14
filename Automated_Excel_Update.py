ec266a21bfc990f8eaa93200e55f4c917a92d210
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-01-14 16:32:35 +1100

    Update Automated_Excel_Update.py

--- Automated_Excel_Update.py
+++ Automated_Excel_Update.py
@@ -76,7 +76,7 @@ def count_dates(workbook, worksheet):
   
     for row in range(row_shifts):
         worksheet.insert_rows(row + row_shift_pos)
-        
+       
     #print("Row ", worksheet.max_column +1)
     for traverse_row in range(row_location, row_location + 1): 
         while column <= worksheet.max_column:
