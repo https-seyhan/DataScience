b015347d3998d83cb58707ccb1543b38ca0277ad
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-03-10 10:43:30 +1100

    Update probabilityModeling.py

--- probabilityModeling.py
+++ probabilityModeling.py
@@ -8,7 +8,7 @@ class probabilityModel:
     def __init__(self):  
         randomVariable = np.random.rand(100)
         self.__plotProbs(randomVariable)
-
+        
     def generateModel(self):
         print("Generate Model Here ")
     
