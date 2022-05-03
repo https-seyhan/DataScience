8e94856fd2f16586c6dd1f3c5a10b7b16f227eee
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-05-03 17:27:30 +1000

    Update linechart.py

--- linechart.py
+++ linechart.py
@@ -25,7 +25,7 @@ for row in sheet.iter_rows(min_row=2,
                           max_col=13):
     for cell in row:
         cell.value = random.randrange(5, 100)
- 
+
 chart = LineChart()
 data = Reference(worksheet=sheet,
                  min_row=2,
